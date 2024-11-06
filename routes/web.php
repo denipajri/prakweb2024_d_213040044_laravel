c37eedf658d3000f8ecc70b0ed445a9ebc108b43
denipajri <radenii2002@gmail.com>
2024-11-06 22:28:58 +0700

    video 5 | blade component

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Deni Pajri']);
+    return view('about', ['name' => 'Deni Pajri', 'title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
-
